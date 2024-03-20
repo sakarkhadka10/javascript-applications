9c44764462b33df0b43e998a83ed658e4eec45e3
Sakar Khadka <khadka.sakar10@gmail.com>
2024-03-20 19:46:32 +0545

    unit converter done

---
+++ unit-converter/app.js
@@ -0,0 +1,31 @@
+const unitNumber = document.getElementById('unit-number');
+const cmConverter = document.getElementById('cm-converter');
+const mConverter = document.getElementById('m-converter');
+const errorMessage = document.getElementById('error-message');
+const resultMessage = document.getElementById('result-message');
+
+
+cmConverter.addEventListener('click', ()=>{
+    const unitValue = unitNumber.value;
+    if(!unitValue){
+        errorMessage.style.display = 'block';
+    }else{
+        errorMessage.style.display = 'none';
+
+        const cm = unitValue * 100;
+        resultMessage.innerText = `${unitValue}M = ${cm}CM`;
+    }
+});
+
+mConverter.addEventListener('click', ()=>{
+    const unitValue = unitNumber.value;
+    if(!unitValue){
+        errorMessage.style.display = 'block';
+    }else{
+        errorMessage.style.display = 'none';
+
+        const m = unitValue / 100;
+        resultMessage.innerText = `${unitValue}CM = ${m}M`;
+    }
+});
+
