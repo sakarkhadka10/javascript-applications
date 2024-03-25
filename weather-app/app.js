4ddd71ea46d4b554f7a6ccb7d3baed84f9e84031
Sakar Khadka <khadka.sakar10@gmail.com>
2024-03-25 21:09:11 +0545

    weather app done

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,34 @@
 const cityName = document.getElementById('city-name');
-const searchBtn = document.getElementById('btn')
-const cityTemp = document.getElementById('city-temp')
+const searchBtn = document.getElementById('search-btn');
+const cityTemp = document.getElementById('city-temp');
+const temp = document.getElementById('temp');
+const humid = document.getElementById('humidity');
+const air = document.querySelector('.wind');
 
 const apiKey = "82e71a1225983ae61c992b337e2b79b0";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&uits=metric&q=bangalore";
-
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
-    let data = await response.json();
-
-   console.log(data);
-    cityTemp.innerText = data.name;
-}
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 
 
-checkWeather();
+searchBtn.addEventListener('click', function(){
+    const cityname = cityName.value.trim();
+
+    if(!cityname){
+        alert('Please Enter Correct City')
+    } else{
+        async function checkWeather(){
+            const response = await fetch(apiUrl +`${cityname}` + `&appid=${apiKey}&units=metric`);
+            let data = await response.json();
+        
+           console.log(data);
+            cityTemp.innerText = data.name;
+            temp.innerText = data.main.temp + ' °C';
+            humid.innerText = data.main.humidity + '%';
+            air.innerText = data.wind.speed + ' km/h'
+
+        }
+        checkWeather();
+        
+    }
+    console.log(cityname);
+});
